f29ca4aefb609349edd618e3d014bd068a4c0b28
ESOLUTIONS\Sathish.Puttappa <sathish.puttappa@trivium-esolutions.com>
2023-05-25 11:54:36 +0530

    Modify the java script function

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const myFunction = () => {
+    var first = document.getElementById("firstname").value;
+    var second = document.getElementById("lastname").value;
+
+    alert("Full Name : "+ first+" "+second )
+
+}
